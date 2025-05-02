33fe0c133521aa33cd5667309926ac7083ac8ff3
alusilco <lucia.silva@factoriaf5.org>
2025-05-03 00:58:17 +0200

    feat: adding contact form

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  
 
-# Ruta principal - vista general de todos los países
+
 @app.route('/')
 def index():
     with open('static/json/emisiones_co2.json', 'r') as f:
@@ -11,30 +12,52 @@ def index():
     
     return render_template('index.html', countries=countries)
 
-# Ruta "About Us" - información del equipo
 @app.route('/about')
 def about():
-    # No necesitamos leer el JSON aquí, porque no mostramos países
     return render_template('about.html')
+
 
 @app.route('/project')
 def project():
-    # No necesitamos leer el JSON aquí, porque no mostramos países
     return render_template('project.html')
 
-# Ruta para ver detalles de un país
 @app.route('/country/<country_code>')
 def country_details(country_code):
     with open('static/json/emisiones_co2.json', 'r') as f:
         countries = json.load(f)
     
-    # Buscar el país por su código (por ejemplo: 'es', 'fr', etc.)
     country = next((c for c in countries if c['code'] == country_code), None)
     
     if country is None:
         return "País no encontrado", 404  
     
     return render_template('country_details.html', country=country)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    address = request.form.get('address')
+    message = request.form.get('message')
+
+    
+    if not all([name, email, address, message]):
+        flash('Todos los campos son obligatorios.')
+        return redirect(url_for('form'))
+
+    
+    return redirect(url_for('thanks'))
+
+# Ruta de agradecimiento
+
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
